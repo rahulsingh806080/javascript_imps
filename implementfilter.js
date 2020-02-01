5d34709dd11a984d4387d24c03045f082848797c
rahul singh <rahulps.singh1@gmail.com>
2020-02-01 12:04:56 +0530

    implement filter function

---
+++ implementfilter.js
@@ -0,0 +1,11 @@
+const selfFilter = function(fn, context) {
+  let arr = Array.prototype.slice.call(this);
+  let filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr.hasOwnProperty(i)) continue;
+    fn.call(context, arr[i], i, this) && filteredArr.push(arr[i]);
+  }
+  return filteredArr;
+};
+
+Array.prototype.selfFilter = selfFilter;
